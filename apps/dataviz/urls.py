ed8da7a6acb063a033fc7c89ebb3918f64bc7a51
Sasha-P <sasha.pazyuk@gmail.com>
2016-10-02 14:58:59 +0300

    add home page title test. add base and home templates.

--- apps/dataviz/urls.py
+++ apps/dataviz/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
+from django.conf.urls import url
 
+from .views import HomeView
+
+app_name = 'dataviz'
 urlpatterns = [
-    # url(r'^admin/', admin.site.urls),
-    url(r'^', include('apps.dataviz.urls')),
+    url(r'^$', HomeView.as_view(), name='home'),
 ]
